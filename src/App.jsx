14733567d64850bba1af7bfd8aa1e697be9d96cf
KUNDAVARAPU-BHARGAVI <bhargavikundavarapu2005@gmail.com>
2025-08-25 10:13:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,10 @@ class App extends Component {
             </tr>
             <tr>
               <td></td>
-              <td><button onClick={()=>this.add()}>ADD</button></td>              
+              <td><button onClick={()=>this.add()}>ADD</button></td>  
+<td><button onClick={()=>this.sub()}>SUB</button></td>  
+              <td><button onClick={()=>this.mul()}>MUL</button></td>
+              <td><button onClick={()=>this.div()}>DIV</button></td>              
             </tr>
           </table>
         </section>
